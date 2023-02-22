a15df63014f7453421ec77e859f2c7ab22516531
Bruce <brucelin0325@yandex.com>
2023-02-22 18:05:20 -0500

    bug fix - convert obj to arr

--- app/Http/Controllers/API/NotionController.php
+++ app/Http/Controllers/API/NotionController.php
@@ -908,7 +908,7 @@ class NotionController extends Controller
         }, $contents);
         $uniqueIds = array_unique($idArray);
         $uniqueSubArrays = array_intersect_key($contents, $uniqueIds);
-        return (array) $uniqueSubArrays;
+        return json_decode(json_encode($uniqueSubArrays), true);
     }
 
 
